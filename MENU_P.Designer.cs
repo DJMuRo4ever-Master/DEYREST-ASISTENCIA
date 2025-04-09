2a9abf8798b9ee976b431fe3dc5f866adb568f81
DJMuRo <deyvidmunozromero@gmail.com>
2025-04-09 17:05:45 -0500

    avance

---
+++ MENU_P.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace ATTEDEYV
+{
+    partial class MENU_P
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MENU_P));
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(510, 137);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // MENU_P
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1284, 661);
+            Controls.Add(label1);
+            Name = "MENU_P";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "REGISTRO DE ASISTENCIA";
+            WindowState = FormWindowState.Maximized;
+            Load += MENU_P_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
---
+++ MENU_P.cs
@@ -0,0 +1,15 @@
+namespace ATTEDEYV
+{
+    public partial class MENU_P : Form
+    {
+        public MENU_P()
+        {
+            InitializeComponent();
+        }
+
+        private void MENU_P_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
